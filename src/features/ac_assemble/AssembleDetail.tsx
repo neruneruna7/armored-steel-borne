1d6c21a19a2c8f6e68e7985d4ffbd4389dcb32fe
neruneruna7 <neruneruna7@gmail.com>
2024-05-11 14:09:06 +0900

    update: asm details

--- src/features/ac_assemble/AssembleDetail.tsx
+++ src/features/ac_assemble/AssembleDetail.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import Image from "next/image";
 
+// ダミーデータ
+import { Frame, acAssembles } from "./ac6Types";
+
+import { Weapons } from "./ac6Types";
+
 // オプションをインポートする
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,82 +14,33 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import styles from "./index.module.css";
 
-type AcAsm = {
-  pilotName: string;
-  mechName: string;
-  emblemUrl: string;
-  mechImages: string[];
-  partsList: string[];
-  description: string;
-  remarks: string;
-};
-
-// {
-//   pilotName,
-//   mechName,
-//   emblemUrl,
-//   mechImages,
-//   partsList,
-//   description,
-//   remarks,
-// }: MechDetailProps)
-
-const acAsm = {
-  pilotName: "V.Ⅳ Rusty",
-  acName: "STEEL HAZE",
-  emblemUrl: "/ac/rusty.jpg",
-  acImages: ["/ac/ac.jpg", "/ac/ac2.jpg", "/ac/ac3.png"],
-  partsList: ["Part 1", "Part 2", "Part 3"],
-  description: "This is a description",
-  remarks: "These are remarks",
-};
-
-const images = [
-  "/ac/ac.jpg",
-  "/ac/ac2.jpg",
-  "/ac/ac3.png",
-];
-
 export default function AssembleDetail() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const handlePrevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? acAsm.acImages.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === acAsm.acImages.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
+  const acAsm = acAssembles[0];
 
   return (
     <div className="min-h-full w-screen flex flex-col justify-center items-center gap-5">
       <div className="flex border-4">
-        <img className="w-32 h-32" src={acAsm.emblemUrl} alt="Emblem" />
+        <img className="w-32 h-32" src={acAsm.emblemImageUrl} alt="Emblem" />
         <div className="m-5 ">
           <h1 className="text-2xl font-bold">AC: {acAsm.acName}</h1>
           <h2 className="text-xl">PILOT: {acAsm.pilotName}</h2>
         </div>
       </div>
-      <ImageSwipe />
-      <ul>
-        {acAsm.partsList.map((part, index) => (
-          <li key={index}>{part}</li>
-        ))}
-      </ul>
-      <p>{acAsm.description}</p>
-      <p>{acAsm.remarks}</p>
-
-
+      <ImageSwipe images={acAsm.acImageUrls} />
+      <WeaponView weapons={acAsm.parts.weapons} />
+      <FrameView frame={acAsm.parts.frame} />
+      <Description description={acAsm.description} />
+      <p>備考：{acAsm.remarks}</p>
     </div>
   );
 }
 
-function ImageSwipe() {
+interface ImageSwipeProps {
+  images: string[];
+}
+
+function ImageSwipe({images}: ImageSwipeProps) {
   return (
     <Swiper
       modules={[Navigation, Pagination, Autoplay]}
@@ -121,21 +77,56 @@ function ImageSwipe() {
 }
 
 
-// export default function AssembleDetail() {
+interface PartViewProps {
+  partType: string;
+  partName: string;
+}
 
-//   return (
-//     <div className="min-h-full py-40 w-full flex flex-col justify-center items-center gap-10">
-//       {/* ...existing code... */}
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-//         <button onClick={handlePrevImage}>Prev</button>
-//         <img
-//           className="w-32 h-32"
-//           src={acAsm.acImages[currentImageIndex]}
-//           alt={`Mech ${currentImageIndex + 1}`}
-//         />
-//         <button onClick={handleNextImage}>Next</button>
-//       </div>
-//       {/* ...existing code... */}
-//     </div>
-//   );
-// }
+function PartView({ partType, partName }: PartViewProps) {
+  return (
+    <div className="flex mb-1">
+      <div className="w-48 text-center border-2 bg-slate-200">
+        <h2>{partType}</h2>
+      </div>
+      <div className="w-72 border-2">
+        <h2>{partName}</h2>
+      </div>
+    </div>
+  )
+}
+
+
+function WeaponView({ weapons }: { weapons: Weapons }) {
+  return (
+    <div>
+      <PartView partType="R-ARM" partName={weapons.rArm} />
+      <PartView partType="L-ARM" partName={weapons.lArm} />
+      <PartView partType="R-BACK" partName={weapons.rBack} />
+      <PartView partType="L-BACK" partName={weapons.lBack} />
+    </div>
+  )
+}
+
+function FrameView({ frame }: { frame: Frame }) {
+  return (
+    <div>
+      <PartView partType="HEAD" partName={frame.head} />
+      <PartView partType="CORE" partName={frame.core} />
+      <PartView partType="ARMS" partName={frame.arms} />
+      <PartView partType="LEGS" partName={frame.legs} />
+    </div>
+  )
+}
+
+
+interface DescriptionProps {
+  description: string;
+}
+
+function Description({ description }: DescriptionProps) {
+  return (
+    <div className="w-3/4 h-fit border-4">
+      <p className="w-full whitespace-pre-line">{description}</p>
+    </div>
+  )
+}
--- src/features/ac_assemble/ac6Types.ts
+++ src/features/ac_assemble/ac6Types.ts
@@ -1,25 +1,94 @@
 // アーマードコア関連のデータ型を定義
 // modelだと機体のモデルとも捉えられてしまいそうなので，Typeという名称を使用
+// shere Rustクレートの作成にともない，仮の型を置いておく場所にする
+// 不要になったタイミングでファイルを削除すること
 
-interface AcAssemble {
+export interface AcAssemble {
     pilotName: string,
     acName: string;
-    acImageUrl: string;
+    acCardImageUrl: string;
     emblemImageUrl: string;
+    acImageUrls: string[]
+    parts: Parts,
+    description: string,
+    remarks: string,
 }
 
-interface Parts {
+export interface Parts {
     weapons: Weapons;
+    frame: Frame
 }
 
-interface Weapons {
+export interface Weapons {
     rArm: string; //腕
     lArm: string;
     rBack: string; // 肩
+    lBack: string;
 }
 
-interface frame {
+export interface Frame {
     head: string,
     core: string,
-    
+    arms: string,
+    legs: string,
 }
+
+// ダミーデータ
+export const acAssembles: AcAssemble[] = [
+    {
+        pilotName: "V.Ⅳ Rusty",
+        acName: "STEEL HAZE",
+        acCardImageUrl: "/ac/steel-haze.webp",
+        emblemImageUrl: "/ac/rusty.jpg",
+        acImageUrls: [
+            "/ac/steel-haze.webp",
+            "/ac/ac.jpg",
+            "/ac/ac2.jpg",
+            "/ac/ac3.png",
+        ],
+        parts: {
+            weapons: {
+                rArm: "MA-E-211 SAMPU",
+                lArm: "Vvc-774LS",
+                rBack: "Vvc-703PM",
+                lBack: "MA-J-200 RANSETSU-RF"
+            },
+            frame: {
+                head: "NACHTREIHER/44E",
+                core: "NACHTREIHER/40E",
+                arms: "NACHTREIHER/46E",
+                legs: "NACHTREIHER/42E",
+            },
+        },
+        description: "アーキバスグループ強化人間部隊 ヴェスパーの第4隊長\n\nラスティはグループ傘下であるシュナイダー社の\n人材公募プログラムで見出され，半年に満たない短期でヴェスパー上位に抜擢された類を見ない経歴の持ち主である．彼は入隊以前に強化手術を受けており，詳細は不明だが本人の申告によると第8世代であるという",
+        remarks: "These are remarks",
+    },
+    {
+        pilotName: "Pilot 2",
+        acName: "Mech 2",
+        acCardImageUrl: "/ac/mech2.jpg",
+        emblemImageUrl: "/ac/emblem2.jpg",
+        acImageUrls: [
+            "/ac/ac.jpg",
+            "/ac/ac2.jpg",
+            "/ac/ac3.png",
+        ],
+        parts: {
+            weapons: {
+                rArm: "Weapon A",
+                lArm: "Weapon B",
+                rBack: "Weapon C",
+                lBack: "Weepon D"
+            },
+            frame: {
+                head: "Head 2",
+                core: "Body 2",
+                arms: "Arms 2",
+                legs: "Legs 2",
+            },
+        },
+        description: "This is another description",
+        remarks: "These are more remarks",
+    },
+    // 必要に応じて他のダミーデータを追加
+];
