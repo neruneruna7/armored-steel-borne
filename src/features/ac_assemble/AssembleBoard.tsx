25aed2e393e6d7e35241ba537a356598aaced522
neruneruna7 <neruneruna7@gmail.com>
2024-05-11 16:08:44 +0900

    add: card to debug link

--- src/features/ac_assemble/AssembleBoard.tsx
+++ src/features/ac_assemble/AssembleBoard.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 // interface AssembleData {
@@ -19,24 +20,26 @@ interface AcAsm {
 
 function AssembleCard({ mech }: { mech: AcAsm }) {
   return (
-    <div className="border-4 w-80 h-72 relative">
-      <div className="flex h-auto border-2">
-        <div className="">
-          <Image src={mech.emblemImageUrl} alt={`Mech Image`} width={70} height={70} className="object-contain border-2" />
-        </div>
-        <div className="ml-4">
-          <h2>PILOT: {mech.acPilotName}</h2>
-          <h2>AC: {mech.acName}</h2>
-        </div>
-        {/* <div className="border-4">
+    // いい感じのホバー時のスタイルがわからん
+    <Link href="/debug" className="hover:mix-blend-luminosity hover:bg-gray-600" >
+      <div className="border-4 w-80 h-72 relative">
+        <div className="flex h-auto border-2">
+          <div className="">
+            <Image src={mech.emblemImageUrl} alt={`Mech Image`} width={70} height={70} className="object-contain border-2" />
+          </div>
+          <div className="ml-4">
+            <h2>PILOT: {mech.acPilotName}</h2>
+            <h2>AC: {mech.acName}</h2>
+          </div>
+          {/* <div className="border-4">
           <p>{mech.description}</p>
         </div> */}
+        </div>
+        <div className="border-2 relative w-full h-56">
+          <Image src={mech.acImageUrl} alt={`Mech Image`} fill className="object-contain" />
+        </div>
       </div>
-
-      <div className="border-2 relative w-full h-56">
-        <Image src={mech.acImageUrl} alt={`Mech Image`} fill className="object-contain" />
-      </div>
-    </div>
+    </Link>
   );
 }
 
@@ -119,13 +122,13 @@ const acData: AcAsm[] = [
 
 
 export default function AssembleBoard() {
-  
+
   const acCards = acData.map((acAsm, index) => (
     <AssembleCard key={index} mech={acAsm} />
   ));
 
   return (
-    <div className="min-h-full py-40 w-full flex flex-col justify-center items-center gap-10">
+    <div className="min-h-full w-full flex flex-col justify-center items-center gap-10">
       <h1>AC Assemble</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {acCards}
