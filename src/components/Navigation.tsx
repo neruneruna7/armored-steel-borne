6d13aae3b49d861362128e8d10c4f1592dad16d4
neruneruna7 <neruneruna7@gmail.com>
2024-05-12 15:04:39 +0900

    update: assemble page to index, index to exampleindex

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,11 +14,14 @@ function Navigation() {
         <li>
           <Link href="/register">Register</Link>
         </li>
-        <li>
+        {/* <li>
           <Link href="/assemble">Assemble</Link>
-        </li>
+        </li> */}
         <li>
           <Link href="/debug">Debug</Link>
+        </li>
+        <li>
+          <Link href="/exampleindex">Example-index</Link>
         </li>
       </ul>
     </nav>
