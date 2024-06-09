a07ab02443ba06a4d6041cad20fdf59012b85bb5
neruneruna7 <neruneruna7@gmail.com>
2024-06-09 11:57:54 +0900

    update: get

--- backend/src/api/ac_assemble/handler/get.rs
+++ backend/src/api/ac_assemble/handler/get.rs
@@ -2,9 +2,13 @@ use http::StatusCode;
 use share::model::assemble_reqres::{AcAsmGetReq, AcAsmGetRes};
 use tracing::info;
 
-use crate::api::ac_assemble::dummy_data;
+use crate::{api::ac_assemble::dummy_data, repository::assemblies::Ac6AssembliesRepo, AppState};
 
-use axum::{extract::Path, response::IntoResponse, Json};
+use axum::{
+    extract::{Path, State},
+    response::IntoResponse,
+    Json,
+};
 
 #[utoipa::path(
 get,
@@ -15,19 +19,26 @@ responses(
     (status = 200, description = "OK", body = AcAsmGetRes),
 ),
 )]
-#[tracing::instrument]
-pub async fn get_ac_asm(Path(req): Path<AcAsmGetReq>) -> impl IntoResponse {
-    // ここでAcAssembleのダミーデータを作成します。
-    let ac_assemble = dummy_data::dumy_assembles();
+#[tracing::instrument(skip(state))]
+pub async fn get_ac_asm(
+    Path(req): Path<AcAsmGetReq>,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, StatusCode> {
+    // // ここでAcAssembleのダミーデータを作成します。
+    // let ac_assemble = dummy_data::dumy_assembles();
 
-    // reqのUlidと一致するAcAssembleを取得
-    // ひとまずエラーハンドリングはせずに動作確認
-    let ac_assemble = ac_assemble
-        .into_iter()
-        .find(|ac_assemble| ac_assemble.id == req)
-        .unwrap();
+    // // reqのUlidと一致するAcAssembleを取得
+    // // ひとまずエラーハンドリングはせずに動作確認
+    // let ac_assemble = ac_assemble
+    //     .into_iter()
+    //     .find(|ac_assemble| ac_assemble.id == req)
+    //     .unwrap();
+
+    // reqのidをもとに，DBからAcAssembleを取得
+    let repo = Ac6AssembliesRepo::new(state.postgres.clone());
+    let ac_assemble = repo.read(req).await.map_err(|_| StatusCode::BAD_REQUEST)?;
 
     info!("ac_assemble name: {:?}", ac_assemble.ac_name);
 
-    (StatusCode::OK, Json(AcAsmGetRes { ac_assemble }))
+    Ok((StatusCode::OK, Json(AcAsmGetRes { ac_assemble })))
 }
